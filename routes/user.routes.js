e89590a5a2fe1fb8a11dba815a29f697596fa6a9
Saeideh Bahadoran <saeideh.bahadoran@gmail.com>
2024-05-03 14:17:23 +0200

    creating the update- delete and get detailes of user in user.routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -6,23 +6,79 @@ const {
   isTeacher,
 } = require("../middlewares/route-guard.middleware");
 
-// /api/courses
+// /api/users
 
-// GET all courses of this teacher
-router.get("/", isAuthenticated, async (req, res) => {
+// GET all users of this teacher
+router.get("/", (req, res) => {
+  res.json("All good in user");
+});
+
+// GET all users
+// router.get("/", isAuthenticated,isTeacher, async (req, res) => {
+//   try {
+//     const user = await User.findById(req.tokenPayload.userId);
+
+//     if (user.isTeacher) {
+//       const allCourses = await User.find({
+//         teacher: req.tokenPayload.userId,
+//       });
+
+//       if (!allCourses.length) {
+//         console.log("There is no course to show");
+//       }
+//       res.status(200).json(allCourses);
+//     } else {
+//       const allCourses = await Course.find({
+//         studentList: req.tokenPayload.userId,
+//       });
+//       if (!allCourses.length) {
+//         console.log("There is no course to show");
+//       }
+//       res.status(200).json(allCourses);
+//     }
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json(error);
+//   }
+// });
+
+// GET  /api/users/:userId  - show detailes of one user
+router.get("/:userId", isAuthenticated, async (req, res) => {
   try {
-    const allCourses = await User.find({ teacher: req.tokenPayload.userId });
-
-    if (!allCourses.length) {
-      console.log("There is no course to show");
-    }
-    res.status(200).json(allCourses);
+    const user = await User.findById(req.params.userId);
+    res.status(200).json(user);
   } catch (error) {
     console.log(error);
     res.status(500).json(error);
   }
 });
 
+//PUT /api/users/:userId - Update a specific user by id
+router.put("/:userId", isAuthenticated, async (req, res) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.userId,
+      req.body,
+      {
+        new: true,
+      }
+    );
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    console.error("Error while updating user ->", error);
+    res.status(500).json({ message: "Error while updating a single user" });
+  }
+});
 
+//DELETE /api/users/:userId - Delete a specific user by id
+router.delete("/:userId", isAuthenticated, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(204).send();
+  } catch (error) {
+    console.error("Error while deleting a user ->", error);
+    res.status(500).json({ message: "Error while deleting a single user" });
+  }
+});
 
-module.exports = router
+module.exports = router;
