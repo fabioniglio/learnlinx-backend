215e8c87c0a8bd7426603ca817281f1337e7dbf4
Fabio Niglio <fabio_niglio@hotmail.com>
2024-11-27 14:54:11 +0100

    remove origin

--- config/index.js
+++ config/index.js
@@ -27,16 +27,16 @@ module.exports = (app) => {
   //     credentials: true,
   //   })
   // );
-  app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*"); // Temporary wildcard
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    if (req.method === "OPTIONS") {
-      res.sendStatus(200);
-    } else {
-      next();
-    }
-  });
+  // app.use((req, res, next) => {
+  //   res.header("Access-Control-Allow-Origin", "*");
+  //   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  //   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  //   if (req.method === "OPTIONS") {
+  //     res.sendStatus(200);
+  //   } else {
+  //     next();
+  //   }
+  // });
 
   // In development environment the app logs
   app.use(logger("dev"));
